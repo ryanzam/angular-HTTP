a572ff9c296941a6b4a9f74e24b8e219c48d7cdc
ryanzam <ryanzam2005@yahoo.com>
2017-10-08 09:58:59 +0300

    http request  jasonholder api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HttpComponentComponent } from './http-component/http-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HttpComponentComponent
   ],
   imports: [
     BrowserModule,
